3a133669cae48e55918cf295d4f723c5cfb781df
ArfricDev <arfricdev@ArfricDevs-iMac.local>
2024-10-02 10:01:02 +0000

    added splash screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -30,6 +30,8 @@ export default function RootLayout() {
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="signup" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
     </ThemeProvider>
